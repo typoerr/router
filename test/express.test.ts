5086bb2d88f3dad750b18f85239fa37a937ec251
typoerr <masahirokato.dev@gmail.com>
2020-05-02 09:56:09 +0900

    test: clean

--- test/express.test.ts
+++ test/express.test.ts
@@ -29,7 +29,7 @@ server.use(async (req, res, next) => {
 
 test('/', (t) => {
   return listen(server, async (url) => {
-    const res = await await fetch(url)
+    const res = await fetch(url)
     t.is(await res.text(), '/')
   })
 })
